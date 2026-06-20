ba8e41d6896ee7b2048b1073e6a966209f307dc6
PunxsyProminence <Admin@punxsyprominence.org>
2026-06-19 23:40:00 -0400

    fix: next.config.mjs ESM

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,22 +1,10 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+﻿import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    cpus: 1,
-    workerThreads: false,
-    staticGenerationMaxConcurrency: 1,
-    staticGenerationMinPagesPerWorker: 100,
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-      '@packages': path.resolve(__dirname, '../../packages'),
+      '@': path.resolve('./src'),
     };
     return config;
   },
